3e8c1f1b8fbae8f11bf263ee6efa9fec4ee1d63c
Asif2501 <asifshah4495@gmail>
2025-05-09 12:53:46 +0530

    forgot api integrate

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import ProfilePage from './components/Profile/ProfilePage';
 import CreateDomain from './components/CreateDomain';
 import Theme2Index from './components/Themes/Theme2/Theme2';
 import ForgotPassword from './components/ForgotPassword';
+import ProtectedRoutes from './ProtectedRoutes';
 
 // Function to check authentication status
 const isAuthenticated = () => !!localStorage.getItem('token');
@@ -19,7 +20,7 @@ const PrivateRoute = ({ element }: { element: React.ReactElement }) => {
 
 function App() {
   const [auth, setAuth] = useState(isAuthenticated());
-
+  const token = localStorage.getItem('token');
   useEffect(() => {
     setAuth(isAuthenticated());
   }, []);
@@ -31,8 +32,21 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login onAuthChange={() => setAuth(isAuthenticated())} />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/chooseTheme/dashboard/:id" element={<PrivateRoute element={<Dashboard />} />} />
-        <Route path="/" element={<Navigate to={auth ? "/chooseTheme" : "/login"} />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/" element={<Navigate to={token ? "/chooseTheme" : "/login"} />} />
+
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/chooseTheme" element={<ChooseThemeIndex />} />
+          <Route path="/chooseTheme/dashboard/:id" element={<Dashboard />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/profile/chooseTheme/dashboard/:id" element={<Dashboard />} />
+          <Route path="/profile/chooseTheme/dashboard" element={<Dashboard />} />
+          <Route path="/profile/chooseTheme/Theme2/:id" element={<Theme2Index />} />
+          <Route path="/profile/chooseTheme/Theme1/:id" element={<Theme2Index />} />
+          <Route path="/profile/Theme2/:id" element={<Theme2Index />} />
+          <Route path="/chooseTheme/subDomainCreate/:id" element={<CreateDomain />} />
+        </Route>
+        {/* <Route path="/chooseTheme/dashboard/:id" element={<PrivateRoute element={<Dashboard />} />} />
         <Route path="/chooseTheme" element={<ChooseThemeIndex />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/profile/chooseTheme/dashboard/:id" element={<Dashboard />} />
@@ -41,8 +55,7 @@ function App() {
         <Route path="/profile/chooseTheme/Theme2/:id" element={<Theme2Index />} />
         <Route path="/profile/chooseTheme/Theme1/:id" element={<Theme2Index />} />
         <Route path="/profile/Theme2/:id" element={<Theme2Index />} />
-        <Route path="/chooseTheme/subDomainCreate/:id" element={<CreateDomain />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/chooseTheme/subDomainCreate/:id" element={<CreateDomain />} /> */}
       </Routes>
       {/* </div>
       </div> */}
